cae0102abb635186c051e542722cda414aa422b9
PavelPodlegaev <pashkevich1617@gmail.com>
2022-06-29 16:17:50 +0300

    git scpipt

---
+++ script github.bat
@@ -0,0 +1,7 @@
+git.exe init
+git.exe add . 
+git.exe commit -a -m "git scpipt"
+git.exe branch -M main
+git.exe remote add origin https://github.com/PavelPodlegaev/Work2.git
+git.exe push -u origin main
+pause
